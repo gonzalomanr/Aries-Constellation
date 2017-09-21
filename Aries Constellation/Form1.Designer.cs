a9ee4ac8990f025a41ad65d72613ec09c7ed378b
temp <temp>
2017-09-21 12:38:55 -0400

    Add project files.

---
+++ Aries Constellation/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Aries_Constellation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AriesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AriesLabel
+            // 
+            this.AriesLabel.AutoSize = true;
+            this.AriesLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.AriesLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AriesLabel.Font = new System.Drawing.Font("Book Antiqua", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AriesLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.AriesLabel.Location = new System.Drawing.Point(63, 284);
+            this.AriesLabel.Name = "AriesLabel";
+            this.AriesLabel.Size = new System.Drawing.Size(189, 26);
+            this.AriesLabel.TabIndex = 0;
+            this.AriesLabel.Text = "Aries Constellation";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(318, 319);
+            this.Controls.Add(this.AriesLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AriesLabel;
+    }
+}
+
---
+++ Aries Constellation/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aries_Constellation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //Graphics
+            Graphics formGraphics = this.CreateGraphics();
+            BackgroundImage = null;
+            Pen drawPen = new Pen(Color.White, 5);
+            SolidBrush drawBrush = new SolidBrush(Color.Yellow);
+            AriesLabel.Dispose();
+            Refresh();
+            formGraphics.Clear(Color.Black);
+
+            //Constellation
+            formGraphics.FillEllipse(drawBrush, 35, 50, 5, 5);
+            formGraphics.FillEllipse(drawBrush, 185, 55, 5, 5);
+            formGraphics.FillEllipse(drawBrush, 325, 75, 5, 5);
+            formGraphics.FillEllipse(drawBrush, 355, 85, 5, 5);
+            formGraphics.FillEllipse(drawBrush, 25, 25, 5, 5);
+            //label1
+        }
+    }
+}
